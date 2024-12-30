4dc553f55ea4ab6f4fe5f9727d79bc5004fea701
viratpk18 <praveenkumar2k4.t@gmail.com>
2024-12-30 16:25:28 +0530

    components added

---
+++ src/Components/About.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+
+function About() {
+    return (
+        <div>
+        <h1>About</h1>
+        <p>This is a simple application to demonstrate React Router.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+        </div>
+    );
+}
+
+export default Home;
